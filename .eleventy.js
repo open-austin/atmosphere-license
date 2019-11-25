2b0d422cf3ac24d31f87080c0121dfc44d6a1711
Matt Carey <matt@authorityspoke.com>
2019-11-25 16:37:14 -0600

    make section lettering copyable

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,7 @@ module.exports = function (eleventyConfig) {
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("js");
+    eleventyConfig.addPassthroughCopy("css");
     eleventyConfig.addPassthroughCopy("favicon.ico");
 
     return {
